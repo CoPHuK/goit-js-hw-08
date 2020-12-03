499e78350888632b0a37e127f1cae9395fa485ac
Aleksandr Kadun <67789706+CoPHuK@users.noreply.github.com>
2020-12-03 12:39:15 +0200

    add function create img

--- js/createEl.js
+++ js/createEl.js
@@ -1,21 +1,31 @@
+import galleryItems from '../gallery-items.js'
 //import galleryList from '../gallery-items'
-//console.log(galleryList);
+const galleryRef = document.querySelector('.js-gallery')
+console.dir(galleryRef);
+console.log(galleryItems);
 const createElement = (element) =>
 { 
     const galleryItem = document.createElement('li')
-    galleryItem.setAttribute('class', 'gallery-item')
+    galleryItem.setAttribute('class', 'gallery__item')
     
     const galleryLink = document.createElement('a')
-    galleryLink.setAttribute('class', 'gallery-link')
+    galleryLink.setAttribute('class', 'gallery__link')
 
     const galletyImg = document.createElement('img')
-    galletyImg.setAttribute('class', 'gallery-img')
+    galletyImg.setAttribute('class', 'gallery__image')
 
     galletyImg.setAttribute('alt', element.description)
     galletyImg.setAttribute('src', element.preview)
-    galletyImg.setAttribute('dara-source', element.original)
+    galletyImg.setAttribute('data-source', element.original)
+    galletyImg.setAttribute('data-index', galleryItems.indexOf(element))
+    galleryLink.appendChild(galletyImg)
+    galleryItem.appendChild(galleryLink)
 
+    return galleryItem
     
 }
 
-createElement()
+const elements = galleryItems.map(createElement)
+console.log(elements);
+galleryRef.append(...elements)
+
